d96fb9b32cf40f461fb56a563d972213325ab0be
rajakswamy <kusome.raja79@gmail.com>
2022-04-01 21:43:12 +0530

    one updated and three new

---
+++ evergreen.sh
@@ -0,0 +1,4 @@
+This file is to have seperate list of evergreen tree species from Andhra Pradesh
+Finally it is not going to take for green development porpose
+Because those plants are not going to grow in Andhra Pradesh
+Thank you
